9f474244d38a2a5f59b21868d26b2bee38e5924f
themischarng <lynncharng@gmail.com>
2020-02-26 10:55:42 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import { Table } from "react-bootstrap";
 
 const API_URL =
-  "https://xsrzrcryliwt908-adw.adb.us-ashburn-1.oraclecloudapps.com/ords/citurria/people/";
+  "https://xsrzrcryliwt908-adw.adb.us-ashburn-1.oraclecloudapps.com/ords/citurria/CS_Master/";
 
 const fetchHeader = new Headers();
 fetchHeader.append("Authorization", "Basic Y2l0dXJyaWE6V2VsY29tZTEyMzQ1");
@@ -36,11 +36,20 @@ function App() {
       <Table variant="dark" striped bordered hover>
         <thead>
           <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Profession</th>
-            <th>Location</th>
+            <th>CS_DATE</th>
+            <th>CS_SEQNO</th>
+            <th>CS_IDNO</th>
+            <th>CS_CARDNO</th>
+            <th>CS_CASETYPE</th>
+			<th>CS_RESULT</th>
+			<th>CS_FINISHDATE</th>
+			<th>CS_USERID</th>
+			<th>CS_CDFLAG</th>
+			<th>CS_UCALL</th>
+			<th>CS_ECDATE</th>
+			<th>CS_REPLY</th>
+			<th>CS_REPLY_PHONE</th>
+			<th>DATA_SRC_DT</th>
           </tr>
         </thead>
         <tbody>
@@ -48,11 +57,20 @@ function App() {
             {
               return (
                 <tr key={`item-${index}`}>
-                  <td>{item.id}</td>
-                  <td>{item.name}</td>
-                  <td>{item.age}</td>
-                  <td>{item.profession}</td>
-                  <td>{item.location}</td>
+                  <td>{item.CS_DATE}</td>
+                  <td>{item.CS_SEQNO}</td>
+                  <td>{item.CS_IDNO}</td>
+                  <td>{item.CS_CARDNO}</td>
+                  <td>{item.CS_CASETYPE}</td>
+				  <td>{item.CS_RESULT}</td>
+				  <td>{item.CS_FINISHDATE}</td>
+				  <td>{item.CS_USERID}</td>
+				  <td>{item.CS_CDFLAG}</td>
+				  <td>{item.CS_UCALL}</td>
+				  <td>{item.CS_ECDATE}</td>
+				  <td>{item.CS_REPLY}</td>
+				  <td>{item.CS_REPLY_PHONE}</td>
+				  <td>{item.DATA_SRC_DT}</td>
                 </tr>
               );
             }
